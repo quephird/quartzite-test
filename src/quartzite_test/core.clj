45f8dbfa4a83bb6c24d0d0c255b6a389b2f3d1b1
quephird <danielle_kefford@hotmail.com>
2017-02-07 23:20:14 -0500

    Added printlns to illustrate what is going on during execution.

--- src/quartzite_test/core.clj
+++ src/quartzite_test/core.clj
@@ -1,27 +1,45 @@
 (ns quartzite-test.core
-  (:gen-class)
-  (:require [clojurewerkz.quartzite.jobs :as jobs]
-            [clojurewerkz.quartzite.scheduler :as scheduler]
-            [clojurewerkz.quartzite.triggers :as triggers]))
+  (:require [clojurewerkz.quartzite.conversion :as conversion]
+            [clojurewerkz.quartzite.jobs :as jobs]
+            [clojurewerkz.quartzite.scheduler :as scheduler]))
 
 (jobs/defjob DurableJob [ctx]
-   (println "I'M IN UR DATABASE DROPPN UR USERS"))
+  (let [job-data (conversion/from-job-data ctx)]
+    (println "Waiting inside job...")
+    (Thread/sleep 3000)
+    (println "Executing job...")
+    (doseq [[k v] job-data]
+      (println (format "Parameter %s is %s" k v)))
+    (println "Exiting job...")))
 
-(def my-job-detail
+(defn make-job-detail [job-id job-data]
   (jobs/build
     (jobs/of-type DurableJob)
     (jobs/store-durably)
+    (jobs/using-job-data job-data)
     (jobs/with-identity
-      (jobs/key "jobs.durable"))))
+      (jobs/key job-id))))
 
-(def my-trigger
-  (triggers/build
-    (triggers/with-identity (triggers/key "triggers.durable"))
-    (triggers/start-now)))
-
-(def my-scheduler
+(defn make-scheduler []
   (scheduler/initialize))
 
 (defn -main []
-  (scheduler/start quartzite-test.core/my-scheduler)
-  (scheduler/schedule my-scheduler my-job-detail my-trigger))
+  (let [scheduler (make-scheduler)
+        job-id "jobs.durable"
+        job-data {:foo "foo" :bar 42}
+        job-detail (make-job-detail job-id job-data)
+        job-key (jobs/key job-id)]
+    (println "Clearing the database from the last run...")
+    (scheduler/clear! scheduler)
+    (println "Starting the scheduler...")
+    (scheduler/start scheduler)
+    (println "Adding the new job to the scheduler...")
+    (scheduler/add-job scheduler job-detail)
+    (println "Triggering the job...")
+    (.triggerJob scheduler job-key)
+
+    ; This forces quartz to start the job before attempting to shut down.
+    (while (not (scheduler/currently-executing-job? scheduler job-id)))
+    (println "Shutting down quartz...")
+    (scheduler/shutdown scheduler true)
+    (println "Exiting program...")))
